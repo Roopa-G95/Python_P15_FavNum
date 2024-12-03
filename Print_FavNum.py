0f4aaa13ba742e8e625bb291caf0968b21cb696a
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-12-03 19:29:34 +0530

    FavNum

---
+++ Print_FavNum.py
@@ -0,0 +1,7 @@
+a=int(input('Enter a Number'))
+if(a==1 or a==10 or a==2 or a==8 or a==3):
+    print(a ,'is your Favorite Number')
+elif(a==7 or a==4 or a==6 or a==5 or a==9):
+    print(a,'is not your Favorite Number')
+else:
+    print('Enter a Valid Number')
